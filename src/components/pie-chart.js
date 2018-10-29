230aa8df30189f156e0b71edef077471e64b2d23
Abdul Hamid HmDi <axmxh@hotmail.com>
2018-10-29 04:56:00 +0200

    clean code and add click event to first chart

--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Charts from './charts';
 import * as V from 'victory';
+import * as data from '../orders-charts.json';
+
 import { VictoryChart, VictoryTheme, VictoryPie, VictoryBar } from 'victory';
+import * as crossfilter from 'crossfilter';
+
 
 class PieChart extends React.Component {
 
@@ -13,6 +17,35 @@ class PieChart extends React.Component {
                     labelRadius={70}
                     style={{ labels: { fill: "white", fontSize: 14, fontWeight: "bold" } }}
                     data={this.props.data}
+                    events={[
+                        {
+                            target: "data",
+                            eventHandlers: {
+                                //onClick: (evt) => alert(`(${evt.target}, ${evt.target})`)
+                                onClick: () => {
+                                    return [{
+                                        target: "labels",
+                                        mutation: (props) => {
+                                            let cross = crossfilter(data);
+                                            let byPayment = cross.dimension(d => d.paymentMethod);
+                                            // let filterByCash = byPayment.filterFunction(d => d == 'Cash').top(Infinity); 
+                                            // let crossCash = crossfilter(filterByCash);
+                                            // let branchByCash = crossCash.dimension(d => d.branch.split(' ')[1]);
+                                            // let groupByPayment =  branchByCash.group();
+                                            // console.log(groupByPayment.all());
+                                            // this.setState({
+                                            //     ordersByBranch: groupByPayment.all()
+                                            // })
+                                            this.props.update(props.text.split('\n')[0]);
+                                            console.log(props.text)
+                                            return props.text === "clicked" ?
+                                                null : { text: "clicked" }
+                                        }
+                                    }];
+                                }
+                            }
+                        }
+                    ]}
                 />
             </div>
         )
